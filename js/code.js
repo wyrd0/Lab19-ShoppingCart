c7ed4dde5e374e5a33dee44643613c8498bc36ed
Mary Olive <mary.olive@me.com>
2016-11-27 08:36:56 -0500

    initial commit

---
+++ js/code.js
@@ -0,0 +1,55 @@
+
+
+
+
+	//var inventory = array["jalapeno", "crunchy peanutbutter","bathtub stopper", "green pepper (ea)", "13 lb turkey"]
+	//var price = array[0.75, 3.00, 5.00, .85, 25.00]
+	
+		 
+
+	function GetCells()
+	{
+
+	}
+	function alertArray()
+	{
+	var inventory = document.getElementById('inventory').value;	
+	var productArray = array.from(inventory);
+	productArray += document.list.value;
+}
+//var priceArray = [];
+
+// function AddToCart()
+// {
+// 	var inventory = document.getElementById('inventory').value;
+// 	var list += document.getElementById("list").value;
+
+// 		for(i = 0; i < inventory.length; i++)
+// 			 {
+// 			 	var product = inventory.innerHTML.tr.td[i].value;
+// 				var cost = inventory.innerHTML.tr.td[i+1].value;
+				
+// 				productArray.
+// 		 		var listIndex += document.getElementById('').value(item + cost)
+// 		 	 }
+
+// 	}
+
+       
+//for (var i = 0; n< tr.length; i++)
+ {
+	
+}
+
+<input = list.tr.innerHTML += "<tr><td>"+product+"</td></tr><tr><td>"+cost+"</td></tr>"
+}
+
+function Sum()
+{
+	var x = document.getElementById("x").value;
+ 	var x = parseInt(x);
+ 	var y = document.getElementById("y").value;
+ 	var y = parseInt(y);
+ 	var sum =(x+y)
+ 	alert(sum)
+}
